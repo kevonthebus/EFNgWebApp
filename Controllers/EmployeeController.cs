af91b7fb43025cd3ac7eced284c03a4675a05355
Kevin Matthews <kevin.matthews@buchanan-edwards.com>
2019-12-27 09:02:08 -0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFNgWebApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFNgWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer dal = new EmployeeDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<TblEmployee> Index()
+        {
+            return dal.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create ([FromBody] TblEmployee employee)
+        {
+            return dal.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public TblEmployee Details (int id)
+        {
+            return dal.GetEmployeeData(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody]TblEmployee employee)
+        {
+            return dal.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return dal.DeleteEmployee(id);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return dal.GetCities();
+        }
+    }
+}
---
+++ Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFNgWebApp.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+
+        FBIDeltaContext _db = new FBIDeltaContext();
+        public IEnumerable<TblEmployee> GetAllEmployees()
+        {
+            try
+            {
+                return _db.TblEmployee.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Add new employee record   
+        public int AddEmployee(TblEmployee employee)
+        {
+            try
+            {
+                _db.TblEmployee.Add(employee);
+                _db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Update the records of a particluar employee  
+        public int UpdateEmployee(TblEmployee employee)
+        {
+            try
+            {
+                _db.Entry(employee).State = EntityState.Modified;
+                _db.SaveChanges();
+
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get the details of a particular employee  
+        public TblEmployee GetEmployeeData(int id)
+        {
+            try
+            {
+                TblEmployee employee = _db.TblEmployee.Find(id);
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Delete the record of a particular employee  
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                TblEmployee emp = _db.TblEmployee.Find(id);
+                _db.TblEmployee.Remove(emp);
+                _db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Get the list of Cities  
+        public List<TblCities> GetCities()
+        {
+            List<TblCities> lstCity = new List<TblCities>();
+            lstCity = (from CityList in _db.TblCities select CityList).ToList();
+
+            return lstCity;
+        }
+
+    }
+}
